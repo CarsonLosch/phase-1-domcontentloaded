af3dc44afe570ad1a3184f15757ac4e84c3cb064
CarsonLosch <95552371+CarsonLosch@users.noreply.github.com>
2022-03-19 16:28:45 -0700

    commit tree a594dbe311328eed831291b17f79f7bb835a54b6

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const addtxt = document.getElementById('text');
+    addtxt.textContent = "This is really cool!"
+  });
+
